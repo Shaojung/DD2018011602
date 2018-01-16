9600150b221018d6b148660a8b31e61ee40c3f45
Teacher <shaojung@gmail.com>
2018-01-16 15:08:37 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018011602/MainActivity.java
@@ -0,0 +1,40 @@
+package tw.com.pcschool.dd2018011602;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("msg0116");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView tv = (TextView) findViewById(R.id.textView);
+                String value = dataSnapshot.getValue(String.class);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void click1(View v)
+    {
+        myRef.setValue("Hello, World!!!!");
+    }
+}
